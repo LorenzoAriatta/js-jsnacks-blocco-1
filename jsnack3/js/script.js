ef951cb013c6b79352a5bea2ccbdffeaf315e0ca
LorenzoAriatta <lorenzotk77@gmail.com>
2022-03-10 18:30:50 +0100

    jsnack3 finito

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -3,22 +3,59 @@ console.log('JS OK!');
 // Chiedi un numero di 4 cifre all’utente
 // calcola la somma di tutte le cifre che compongono il numero
 
-const calculate = document.getElementById('sum-btn');
+
+/*let value = inputNumber;
+console.log(value);
+sum = value
+    .toString()
+    .split('')
+    .map(Number)
+    .reduce(function (a, b) {
+        return a + b;
+    }, 0);
+
+console.log(sum);
 
 
-calculate.addEventListener('click', function () {
 
-    let inputNumber = parseInt(document.getElementById('inputNumber'));
-    let value = inputNumber.value;
-    let sum = 0;
-    console.log(value);
 
-    while (value) {
-        sum += value % 10;
-        value = Math.floor(value / 10);
+function sumDigits() {
+
+    let inputNumber = parseInt(document.getElementById('inputNumber')).value;
+    sum = 0;
+
+    while (inputNumber) {
+        sum += inputNumber % 10;
+        inputNumber = Math.floor(inputNumber / 10);
+
     }
 
     let finalResult = document.getElementById('result');
     finalResult.value = sum;
 
+}*/
+
+
+
+
+let inputNumber = document.getElementById('numeriInseriti');
+
+let totale = document.getElementById('totale');
+
+let calculate = document.getElementById('calculate');
+
+calculate.addEventListener('click', function () {
+    let sum = 0;
+    let userNumber = parseInt(prompt('Inserisci un numero di quattro cifre'));
+    inputNumber.innerText = `Numeri inseriti: ${userNumber}`;
+    while (userNumber) {
+        sum += userNumber % 10;
+        userNumber = Math.floor(userNumber / 10);
+    }
+
+    totale.innerText = `Il risultato è: ${sum}`;
+
 })
+
+
+
