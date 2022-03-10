3da542ff558c1d036511eba2d1bc4fe040507f55
LorenzoAriatta <lorenzotk77@gmail.com>
2022-03-10 23:06:54 +0100

    add css part | little change js part

--- jsnack3/js/script.js
+++ jsnack3/js/script.js
@@ -4,7 +4,8 @@ console.log('JS OK!');
 // calcola la somma di tutte le cifre che compongono il numero
 
 
-/*let value = inputNumber;
+/*
+let value = inputNumber;
 console.log(value);
 sum = value
     .toString()
@@ -13,26 +14,6 @@ sum = value
     .reduce(function (a, b) {
         return a + b;
     }, 0);
-
-console.log(sum);
-
-
-
-
-function sumDigits() {
-
-    let inputNumber = parseInt(document.getElementById('inputNumber')).value;
-    sum = 0;
-
-    while (inputNumber) {
-        sum += inputNumber % 10;
-        inputNumber = Math.floor(inputNumber / 10);
-
-    }
-
-    let finalResult = document.getElementById('result');
-    finalResult.value = sum;
-
 }*/
 
 
@@ -45,17 +26,23 @@ let totale = document.getElementById('totale');
 let calculate = document.getElementById('calculate');
 
 calculate.addEventListener('click', function () {
+
     let sum = 0;
+
     let userNumber = parseInt(prompt('Inserisci un numero di quattro cifre'));
+
     inputNumber.innerText = `Numeri inseriti: ${userNumber}`;
+    inputNumber.style.color = 'green';
+
     while (userNumber) {
+
         sum += userNumber % 10;
+
         userNumber = Math.floor(userNumber / 10);
+
     }
 
     totale.innerText = `Il risultato è: ${sum}`;
+    totale.style.color = 'crimson';
 
 })
-
-
-
