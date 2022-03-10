2266473cf6995dd72e3b68a7fdfc55d1a4d25616
LorenzoAriatta <lorenzotk77@gmail.com>
2022-03-10 16:00:56 +0100

    jsnack1 finito

--- jsnack1/js/script.js
+++ jsnack1/js/script.js
@@ -1,1 +1,39 @@
 console.log('JS OK!');
+
+// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby
+// chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+
+let userName = document.getElementById('userName');
+const btnAccess = document.getElementById('btnAccess');
+
+const invitati = ["aldo", "giovanni", "giacomo", "al", "john", "jack"];
+
+
+btnAccess.addEventListener('click', function () {
+
+    let nameInput = userName.value;
+
+    let vaildName = false;
+
+    if (nameInput) {
+
+        for (let i = 0; i < invitati.length; i++) {
+
+
+            if (nameInput.trim().toLowerCase() === invitati[i]) {
+
+                vaildName = true;
+
+            }
+        }
+
+    }
+
+    if (vaildName) {
+        console.log('Benvenuto alla festa!!')
+    } else {
+        console.log('Mi dispice, ma non sei sulla lista!')
+    }
+
+
+})
